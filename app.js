1b8f9e5154b565114519193019d3c6390cde1be0
Jacob Knecht <jacobknecht3@gmail.com>
2019-07-27 12:27:11 -0500

    installed bcryptjs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const morgan = require('morgan');
 const { sequelize, models } = require('./models');
 const { User, Course } = require('./models');
+const bcryptjs = require('bcryptjs');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
